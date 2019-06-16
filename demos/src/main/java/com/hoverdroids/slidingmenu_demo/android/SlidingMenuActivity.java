2f3edd02d77c6a216a5fc155f1fa6149d2a9c33e
Chris Sprague <spragucm@gmail.com>
2019-06-16 19:17:48 -0400

    Post project reset ... trying to get the subprojects to build

--- demos/src/main/java/com/hoverdroids/slidingmenu_demo/android/SlidingMenuActivity.java
+++
@@ -1,6 +0,0 @@
-package com.hoverdroids.slidingmenu_demo.android;
-
-import com.hoverdroids.slidingmenu.activity.SlidingActivity;
-
-public class SlidingMenuActivity extends SlidingActivity {
-}
--- slidingmenu/src/androidTest/java/com/hoverdroids/slidingmenu/ExampleInstrumentedTest.java
+++ slidingmenu/src/androidTest/java/com/hoverdroids/slidingmenu/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.hoverdroids.hoverdroids_slidingmenu.test", appContext.getPackageName());
+        assertEquals("com.hoverdroids.slidingmenu.test", appContext.getPackageName());
     }
 }
