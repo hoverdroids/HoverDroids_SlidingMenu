a2ba3fddfe58c681bad3edefd85ed46656146f27
Chris Sprague <spragucm@gmail.com>
2019-06-16 19:14:43 -0400

    Right before creating a new project because subprojects are not being built :(

---
+++ demos/src/main/java/com/hoverdroids/slidingmenu_demo/android/SlidingMenuActivity.java
@@ -0,0 +1,6 @@
+package com.hoverdroids.slidingmenu_demo.android;
+
+import com.hoverdroids.slidingmenu.activity.SlidingActivity;
+
+public class SlidingMenuActivity extends SlidingActivity {
+}
--- demos/src/main/java/com/hoverdroids/slidingmenu_demo/kt/MainActivity.kt
+++ demos/src/main/java/com/hoverdroids/slidingmenu_demo/kt/MainActivity.kt
@@ -1,8 +1,9 @@
-package com.hoverdroids.slidingmenu_demo
+package com.hoverdroids.slidingmenu_demo.kt
 
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.hoverdroids.slidingmenu_demo.R
 import kotterknife.bindView
 
 class MainActivity : AppCompatActivity() {
